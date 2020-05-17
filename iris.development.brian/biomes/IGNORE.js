405e66bc102210c2dddeeabe6bf3f81e0cea2d18
NextdoorPsycho <brianneumann630@gmail.com>
2020-05-16 23:14:45 -0700

    Fixing my env

---
+++ iris.development.brian/biomes/IGNORE.js
@@ -0,0 +1,58 @@
+{
+    "name": "Plains",
+    "layers": [
+        {
+            "palette": [
+                "GRASS_BLOCK"
+            ]
+        },
+        {
+            "palette": [
+                "DIRT"
+            ]
+        },
+        {
+            "minHeight": 1, 
+            "maxHeight": 3,
+            "palette": [
+                "DIRT",
+                "DIRT",
+                "COARSE_DIRT"
+            ]
+        }
+    ],
+    "objects": [
+        {
+            "place": ["LargeBoi"],
+            "chance": 0.03,
+            "translate": {
+                "y": -2
+            },
+            "rotation": {
+                "enabled": true,
+                "xAxis": {
+                    "enabled": true,
+                    "min": -23,
+                    "max": 23,
+                    "interval": 1
+                },
+                "yAxis": {
+                    "enabled": true,
+                    "min": 0,
+                    "max": 0,
+                    "interval": 90
+                },
+                "zAxis": {
+                    "enabled": true,
+                    "min": -23,
+                    "max": 23,
+                    "interval": 1
+                }
+            }
+        }
+    ],
+    "derivative": "PLAINS",
+    "heightExponent": 1,
+    "highHeight": 7,
+    "lowHeight": 1
+}
